0e904e5aec09fbafa292a6460c7adef6b40e68b2
Christoph Bruecke <christoph.bruecke@campus.tu-berlin.de>
2011-12-10 21:07:56 +0100

    Initial project setup

---
+++ src/main/java/de/tu_berlin/textmining/translator/prototypes/App.java
@@ -0,0 +1,13 @@
+package de.tu_berlin.textmining.translator.prototypes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
