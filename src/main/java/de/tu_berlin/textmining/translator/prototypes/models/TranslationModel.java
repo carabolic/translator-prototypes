af4a122d79663f32220ac7a215f91fb7063b457a
Christoph Brücke <christoph.bruecke@campus.tu-berlin.de>
2012-03-21 18:00:22 +0100

    Initial work for IBM Translation Model 1

---
+++ src/main/java/de/tu_berlin/textmining/translator/prototypes/models/TranslationModel.java
@@ -0,0 +1,13 @@
+package de.tu_berlin.textmining.translator.prototypes.models;
+
+import edu.berkeley.nlp.lm.NgramLanguageModel;
+
+public class TranslationModel {
+	
+	private NgramLanguageModel<String> languageModel;
+	
+	public TranslationModel()	 {
+		
+	}
+	
+}
